91ca25006734a7155dfdbd793824a003dc4bd48e
HawoonShin <sinhaoon@gmail.com>
2024-11-19 13:03:46 +0900

    02번 문제 제출
    02번 문제 답안 제출

--- Q_02/Assets/Scripts/PlayerStatus.cs
+++ Q_02/Assets/Scripts/PlayerStatus.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class PlayerStatus : MonoBehaviour
 {
-    public float MoveSpeed
-    {
-        get => MoveSpeed;
-        private set => MoveSpeed = value;
-    }
+    public float MoveSpeed { get; set; }
+    // 밑에 방식으로 하면 계속 스택이 쌓이는데 굳이 밑에처럼 할 이유가 있나..?
+    //{
+    //    get => MoveSpeed;
+    //    private set => MoveSpeed = value;
+    //}
 
     private void Awake()
     {
